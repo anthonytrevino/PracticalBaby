cf654f11d14e9cc5081b1e09d32255f7bc93c391
Anthony Trevino <anthonytrevinolol@yahoo.com>
2019-01-04 12:21:26 -0600

    revised login/register components

--- practicalbaby/src/Components/LogIn/LogIn.js
+++ practicalbaby/src/Components/LogIn/LogIn.js
@@ -5,17 +5,18 @@ class LogIn extends Component {
     render() {
         return (
           <div>
-            <form>
+            <form id="loginform">
               <h1>Log In</h1>
               <ul id="registration">
 
-                  <li>E-mail address  <input class="form-input" type="email" placeholder="Isabelle@gmail.com" name="emailaddress" required /></li>
-                  <li>Username  <input class="form-input" id="txt-input" type="text" placeholder="@Username" required /></li>
-                  <li>Password  <input class="form-input" type="password" placeholder="Password" name="password" required /></li>
-                  <li>Confirm Password <input class="form-input" type="password" placeholder="Password" name="password" required /></li>
+                  <li>E-mail address  <input className="form-input" type="email" placeholder="Isabelle@gmail.com" name="emailaddress" required /></li>
+                  <li>Username  <input className="form-input" id="txt-input" type="text" placeholder="@Username" required /></li>
+                  <li>Password  <input className="form-input" type="password" placeholder="Password" name="password" required /></li>
 
-                  <li><button type="submit">Register</button> </li>
+                  <li><input type="checkbox" name="rememberme"/>Remember me?</li>
+                  <li><button type="submit">Log In</button> </li>
               </ul>
+                  <button type="submit">Need an account?</button>
             </form>
           </div>
         )
--- practicalbaby/src/Components/Register/Register.js
+++ practicalbaby/src/Components/Register/Register.js
@@ -5,23 +5,25 @@ class Register extends Component {
     render() {
         return (
             <div>
-                  <form>
+                  <form id="registerform">
 
                     <h1>Register</h1>
                     <ul id="registration">
 
-                    <li>E-mail address  <input class="form-input" type="email" placeholder="Isabelle@gmail.com" name="emailaddress" required /></li>
-                    <li>First Name  <input class="form-input" type="text" placeholder="Isabelle" name="firstname" required /></li>
-                    <li>Last Name  <input class="form-input" type="text" placeholder="Smith" name="firstname" required /></li>
-                    <li>Username  <input class="form-input" id="txt-input" type="text" placeholder="@Username" required /></li>
-                    <li>Password  <input class="form-input" type="password" placeholder="Password" name="password" required />
-                      <span><i class="fa fa-eye" aria-hidden="true" type="button" id="eye"></i></span>
-                      </li>
-                    <li>Confirm Password <input class="form-input" type="password" placeholder="Password" name="password" required /></li>
+                    <li>E-mail address  <input className="form-input" type="email" placeholder="Isabelle@gmail.com" name="emailaddress" required /></li>
+                    <li>First Name  <input className="form-input" type="text" placeholder="Isabelle" name="firstname" required /></li>
+                    <li>Last Name  <input className="form-input" type="text" placeholder="Smith" name="firstname" required /></li>
+                    <li>Username  <input className="form-input" id="txt-input" type="text" placeholder="@Username" required /></li>
+                    <li>Password  <input className="form-input" type="password" placeholder="Password" name="password" required /></li>
+                    <li>Confirm Password <input className="form-input" type="password" placeholder="Password" name="password" required /></li>
 
                     <li><button type="submit">Register</button> </li>
                     </ul>
+                    <button type="submit">I have an account</button>
                   </form>
+                  <div>
+
+                  </div>
             </div>
         )
     }
