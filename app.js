dee19025cd9a299dd121524da576e69314dcedb1
Anthony Trevino <anthonytrevinolol@yahoo.com>
2019-01-10 16:02:38 -0600

    small change

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const session = require('express-session')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const saltRounds = 10;
+//goes at very top of app.js/app.js
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config()
+}
+
+
+
 
 
 
@@ -101,6 +108,18 @@ app.get('/profile/:id', (req, res) => {
     }
 })
 
+
+if (process.env.NODE_ENV === 'production') {
+  // if the client is a create-react-app, go to the .gitignore in the client folder, and take out
+  // the word 'build' so that it isn't hidden from git and heroku
+
+  // serves up the static files
+  app.use(express.static('client/build'))
+  // if the app is a single page app, like a react app that uses react router for example
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  )
+}
 
 const PORT = process.env.PORT || 5000
 
