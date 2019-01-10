22278ebaf9cb03da8fca1055936cccdd67e126a4
Anthony Trevino <anthonytrevinolol@yahoo.com>
2019-01-10 10:56:54 -0600

    added color scheme to homepage

--- practicalbaby/src/Components/Testimonial/Testimonial.js
+++ practicalbaby/src/Components/Testimonial/Testimonial.js
@@ -4,7 +4,7 @@ class Testimonial extends Component {
 
   render() {
     return (
-      <div class="testimonials">
+      <div className="testimonials">
         <h1>Testimonials go here</h1>
         <ul id="testimonialsul">
           <li className="testimonial"><h1>Review #1</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tellus elementum sagittis vitae. Odio pellentesque diam volutpat commodo. </p></li>
