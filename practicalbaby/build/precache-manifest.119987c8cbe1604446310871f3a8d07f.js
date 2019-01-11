2205c497216ab5e737087ce7d1906027d81ac322
Anthony Trevino <anthonytrevinolol@yahoo.com>
2019-01-11 10:17:35 -0600

    removed builds from gitignore

---
+++ practicalbaby/build/precache-manifest.119987c8cbe1604446310871f3a8d07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "171cfcc87e17671c71b2",
+    "url": "/static/js/main.171cfcc8.chunk.js"
+  },
+  {
+    "revision": "c24920efe0ad830bf6ec",
+    "url": "/static/js/1.c24920ef.chunk.js"
+  },
+  {
+    "revision": "171cfcc87e17671c71b2",
+    "url": "/static/css/main.d059d577.chunk.css"
+  },
+  {
+    "revision": "c24920efe0ad830bf6ec",
+    "url": "/static/css/1.bad3ca75.chunk.css"
+  },
+  {
+    "revision": "2bbb74cca0dd2534ccb534cfc6a5129b",
+    "url": "/index.html"
+  }
+];
