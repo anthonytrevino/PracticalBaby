37d7d4fc2494ec644ede796031560b3eaf7a12d8
Pamela Hutchinson <pamelahutchinson@Pamelas-MacBook-Pro.local>
2019-02-19 16:46:48 -0600

    fixed error content and unused content warnings

--- practicalbaby/src/Components/AboutUs/AboutUs.js
+++ practicalbaby/src/Components/AboutUs/AboutUs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './AboutUs.css'
+
 
 class AboutUs extends Component {
   render() {
--- practicalbaby/src/Components/Questionnaire/Questionnaire.js
+++ practicalbaby/src/Components/Questionnaire/Questionnaire.js
@@ -1,20 +1,20 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import BrandQuestionnaire from '../Questionnaire/BrandQuestionnaire';
 import CarSeatQuestionnaire from '../Questionnaire/CarSeatQuestionnaire';
 import ColorQuestionnaire from '../Questionnaire/ColorQuestionnaire';
 import WeightQuestionnaire from '../Questionnaire/WeightQuestionnaire';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import './questionnaire.css';
 
-class Questionnaire extends Component{
+class Questionnaire extends Component {
 
-// constructor(props){
-//   super(props)
-// }
+  // constructor(props) {
+  //   super(props)
+  // }
 
-  render(){
-    let steps = [<BrandQuestionnaire />,<CarSeatQuestionnaire />,<ColorQuestionnaire />,<WeightQuestionnaire />]
-    return(
+  render() {
+    let steps = [<BrandQuestionnaire />, <CarSeatQuestionnaire />, <ColorQuestionnaire />, <WeightQuestionnaire />]
+    return (
       <div>
 
         {steps[this.props.step]}
@@ -26,36 +26,36 @@ class Questionnaire extends Component{
 }
 
 const mapBrandStateToProps = (state) => {
-  return{
+  return {
     isBrandSelected: state.isBrandSelected,
     brandSelected: state.brandSelected,
     step: state.step
   }
 }
 
-const mapCarSeatStateToProps = (state) => {
-  return{
-    isCarSeatTypeSelected: state.isCarSeatTypeSelected,
-    carSeatType: state.carSeatType,
-    step: state.step
-  }
-}
+// const mapCarSeatStateToProps = (state) => {
+//   return {
+//     isCarSeatTypeSelected: state.isCarSeatTypeSelected,
+//     carSeatType: state.carSeatType,
+//     step: state.step
+//   }
+// }
 
-const mapColorStateToProps = (state) => {
-  return{
-    isColorSelected: state.isColorSelected,
-    colorSelected: state.colorSelected,
-    step: state.step
-  }
-}
+// const mapColorStateToProps = (state) => {
+//   return {
+//     isColorSelected: state.isColorSelected,
+//     colorSelected: state.colorSelected,
+//     step: state.step
+//   }
+// }
 
-const mapWeightStateToProps = (state) => {
-  return{
-    isMaxChildWeightSelected: state.isMaxChildWeightSelected,
-    maxChildWeight: state.maxChildWeight,
-    // step: state.step
-  }
-}
+// const mapWeightStateToProps = (state) => {
+//   return {
+//     isMaxChildWeightSelected: state.isMaxChildWeightSelected,
+//     maxChildWeight: state.maxChildWeight,
+//     // step: state.step
+//   }
+// }
 
 // connect(mapStateToProps,mapDispatchToProps)
-export default connect(mapBrandStateToProps,null)(Questionnaire);
+export default connect(mapBrandStateToProps, null)(Questionnaire);
